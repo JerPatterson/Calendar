d4f80fc82daa187754750bd999579c9fcead1548
JerPatterson <jeremie.patterson@polymtl.ca>
2022-03-04 15:21:24 -0500

    starts definition of the class date

--- calendar.h
+++ calendar.h
@@ -1,1 +1,21 @@
 #pragma once
+
+enum class DaysOfWeek {
+	UNDEFINED, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+};
+
+enum class Months {
+	UNDEFINED, JANUARY, FEBUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+};
+
+
+class Date {
+public:
+	Date() = default;
+
+private:
+	int number_ = 4;
+	Months month_ = Months::MARCH;
+	int year_ = 2022;
+	DaysOfWeek dayOfWeek_ = DaysOfWeek::FRIDAY;
+};
